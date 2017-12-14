3508eadbaafb08c1693b971c7ad8a3704fa4488e
J0hn8uff3r <bufferoverflow@hotmail.es>
2017-12-14 05:08:42 +0100

    Update Kindle_Ads_Remover.bat

--- Kindle_Ads_Remover.bat
+++ Kindle_Ads_Remover.bat
@@ -3,12 +3,16 @@ cls
 echo Connect your Kindle and check the drive letter
 set /p letter= "Enter your Kindle device drive letter and press ENTER 'ex: e' "
 set path=%letter%:\system\.assets
-rd %path% /S /Q
-type nul > %path%
+REM rd %path% /S /Q
+REM type nul > %path%
 attrib -r -s -h /S /D %path%
 
 if exist %path%\* (
 	msg * Ha ocurrido un error, los anuncios de tu Kindle no se han podido eliminar correctamente.
+	cls
+	echo Ha ocurrido un error, los anuncios de tu Kindle no se han podido eliminar correctamente.
 ) else (
 	msg * Los anuncios de tu Kindle se han eliminado correctamente.
+	cls
+	echo Los anuncios de tu Kindle se han eliminado correctamente.
 )
